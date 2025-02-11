993813226b8e7748d772c6fc1d2b4878994e6470
Ubuntu <ubuntu@ip-10-0-2-193.us-east-2.compute.internal>
2025-02-11 00:52:03 +0000

    Run Neuron Unit tests with HLO generated per test

---
+++ unit_test.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+TEST_ARTIFACTS_PATH="/shared/aahila/test_artifacts"
+rm -rf "$TEST_ARTIFACTS_PATH"
+mkdir -p "$TEST_ARTIFACTS_PATH"
+NEURON_DUMP_PATH=${TEST_ARTIFACTS_PATH}/neuron_dump
+HLO_DUMP_PATH=${TEST_ARTIFACTS_PATH}/hlo_dump
+export XLA_FLAGS="--xla_dump_hlo_as_text --xla_disable_hlo_passes=aws_neuron_flip_all_gather_dot,neuron-hierarchical-collectives --xla_dump_to=${HLO_DUMP_PATH} --xla_dump_hlo_pass_re='.*'"
+
+# PJRT Flags 
+export NEURON_FSDP_NUM_LAYER_EARLY_AG_SHIFT=1
+export NEURON_FSDP_NUM_LAYER_LATE_RS_SHIFT=2
+export NEURON_ENABLE_INT_MATMUL_DOWNCAST=1
+export NEURON_FSDP=0
+export NEURON_FSDP_NUM_LAYER_COALESCE=-1
+export NEURON_RUN_TRIVIAL_COMPUTATION_ON_CPU=1
+
+# Neuron runtime flags
+export NEURON_RT_DBG_CC_DMA_PACKET_SIZE=4096 && export NEURON_RT_DBG_DMA_PACKETIZATION_SIZE=104857
+export NEURON_RT_ASYNC_EXEC_MAX_INFLIGHT_REQUESTS=1
+export NEURON_RT_IO_RING_CACHE_SIZE=0
+export NEURON_RT_ENABLE_MEMORY_METRICS=0
+export NEURON_RT_VIRTUAL_CORE_SIZE=2
+export NEURON_RT_RESET_CORES=1
+export NEURON_RT_LOG_LEVEL="WARNING"
+export NEURON_RT_ENABLE_INTERNODE_EXECUTION_BARRIER=1
+
+# Neuron collectives flag
+export FI_LOG_LEVEL="warn"
+export OFI_NCCL_PROTOCOL=RDMA
+export LD_LIBRARY_PATH="/opt/amazon/efa/lib/"
+export FI_EFA_USE_DEVICE_RDMA="1"
+export FI_PROVIDER="efa"
+export FI_EFA_FORK_SAFE=1
+export OFI_NCCL_MR_CACHE_DISABLE=1
+
+# Neuron compiler flags
+export NEURON_CC_FLAGS="--framework=XLA"
+export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --internal-max-instruction-limit=20000000"
+export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --target=trn2"
+export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --internal-num-neuroncores-per-sengine=2"
+export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --model-type transformer"
+export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --no-internal-hlo-remat"
+export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --enable-mixed-precision-accumulation"
+export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} -O1"
+export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --tensorizer-options='--enable-hoist-fsdp-collectives'"
+export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --internal-hlo2tensorizer-options='--remat-rope'"
+export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --dump=${NEURON_DUMP_PATH}"
+
+# JAX Cache
+export JAX_COMPILATION_CACHE_DIR="/shared/aahila/compiler/cache/"
+mkdir -p ${JAX_COMPILATION_CACHE_DIR}
+
+echo "setup env vars"
+pytest $1
+
