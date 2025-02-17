7885ed901bbf9d693752b079c0a828fbff0a4135
(AWS) Rahul Huilgol <rahulhuilgol@gmail.com>
2025-02-17 12:21:34 -0800

    Add gather based token dispatch and combine (#1)
    * Add gather based impl
    
    * Add gather based impl, seems to run end to end. Some unit tests have
    failures though. Haven't tested yet on trn2
    
    * Minor changes in test
    
    * Remove print and remove a broadcast in test
    
    * Fix misc issues in MoE gating impl, correctness test still fails
    
    * Fix outer batch issue, and add some annotations
    
    * Improve runner script to run envy dense

--- runner.sh
+++ runner.sh
@@ -37,7 +37,9 @@ TEST_ARTIFACTS_PATH="${ARTIFACTS_PATH}/${JOB_ID}"
 mkdir -p "$TEST_ARTIFACTS_PATH"
 NEURON_DUMP_PATH=${TEST_ARTIFACTS_PATH}/neuron_dump
 HLO_DUMP_PATH=${TEST_ARTIFACTS_PATH}/hlo_dump
-export XLA_FLAGS="--xla_dump_hlo_as_text --xla_disable_hlo_passes=aws_neuron_flip_all_gather_dot,neuron-hierarchical-collectives --xla_dump_to=${HLO_DUMP_PATH} --xla_dump_hlo_pass_re='.*'"
+export XLA_FLAGS="--xla_dump_hlo_as_text --xla_dump_hlo_as_proto --xla_disable_hlo_passes=aws_neuron_flip_all_gather_dot,neuron-hierarchical-collectives --xla_dump_to=${HLO_DUMP_PATH} --xla_dump_hlo_pass_re='.*'"
+
+export MIXTRAL_MOE=$1
 
 # PJRT Flags 
 export NEURON_FSDP_NUM_LAYER_EARLY_AG_SHIFT=1
@@ -46,6 +48,7 @@ export NEURON_ENABLE_INT_MATMUL_DOWNCAST=1
 export NEURON_FSDP=0
 export NEURON_FSDP_NUM_LAYER_COALESCE=-1
 export NEURON_RUN_TRIVIAL_COMPUTATION_ON_CPU=1
+export NEURON_HLO_ANALYZER=1
 
 # Neuron runtime flags
 export NEURON_RT_DBG_CC_DMA_PACKET_SIZE=4096 && export NEURON_RT_DBG_DMA_PACKETIZATION_SIZE=104857
@@ -79,6 +82,10 @@ export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --tensorizer-options='--enable-hoist-
 export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --internal-hlo2tensorizer-options='--remat-rope'"
 export NEURON_CC_FLAGS="${NEURON_CC_FLAGS} --dump=${NEURON_DUMP_PATH}"
 
+# use to add debug logging at module level in xla
+# export TF_CPP_MIN_LOG_LEVEL=0
+# export TF_CPP_VMODULE="gather_scatter_handler=5"
+
 # JAX Cache
 export JAX_COMPILATION_CACHE_DIR="cache/"
 mkdir -p ${JAX_COMPILATION_CACHE_DIR}
@@ -88,7 +95,7 @@ deactivate || true
 # eval "$(/fsx/apoorvgu/conda/bin/conda shell.bash hook)"
 # conda activate py310
 
-source /fsx/huilgolr/jax_fs/bin/activate
+source /fsx/huilgolr/jaxmoe/bin/activate
 
 echo "Listing apt dependencies"
 apt list --installed | grep neuron
@@ -118,7 +125,7 @@ fi
 OUTPUT_DIR="${TEST_ARTIFACTS_PATH}/axlearn_out"
 mkdir -p ${OUTPUT_DIR}
 DATA_DIR="gs://axlearn-public/tensorflow_datasets"
-#fuji-7B-v2-flash
+# fuji-7B-v2-flash
 python -m axlearn.common.launch_trainer_main \
 	    --module=text.gpt.c4_trainer --config=envy-Mistral-8x7B \
 	        --trainer_dir=$OUTPUT_DIR --data_dir=$DATA_DIR \
