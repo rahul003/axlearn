c4eb810f85be441c4967ebd46f962266084a274c
(AWS) Rahul Huilgol <rahulhuilgol@gmail.com>
2025-10-24 20:02:54 -0700

    Fixcorrectness (#51)
    * Fix correctness issue in gating test, but fp32 golden produced
    
    * Fix for index calc, cpu run passes. trn run OOB
    
    * Fixed test
    
    * Cleanup

--- aws-testing/gating_test.py
+++ aws-testing/gating_test.py
@@ -42,7 +42,7 @@ class TestDev150bGatingUnit(GatingTestCase):
             top_k=2,
             capacity_factor=2,
             mesh_spec={"fsdp": -1, "model": 16},
-            batch=8,
+            batch=4,
             seq=8192,
             dtype=jnp.bfloat16,
         )[1]
@@ -53,30 +53,29 @@ class TestDev150bGatingUnit(GatingTestCase):
     def test_unit_fwd_blockwisev2(self):
         self.helper_blockwise_gating(self.create_cfg(test=TopKGatingGatherBlockwiseV2, golden=None, test_device="cpu", layer="gating"))
 
-    def test_unit_fwd_blockwisev2_ep(self):
-        cfg = create_test_config(
-            layer="gating",
-            test=TopKGatingGatherBlockwiseV2,
-            golden=TopKGatingGatherBlockwise,
-            golden_device="cpu",
-            test_device="cpu",
+class TestSwitchBaseGatingUnit(GatingTestCase):
+    def create_cfg(self, test, golden, test_device, golden_device="cpu", layer="gating"):
+        return create_test_config(
+            layer=layer,
+            test=test,
+            golden=golden,
+            golden_device=golden_device,
+            test_device=test_device,
             input_dim=1024,
             hidden_dim=4096,
             n_experts=64,
             n_groups=1,
-            top_k=1,
-            capacity_factor=1,
-            mesh_spec={"fsdp": -1, "model": 4, "seq": 4, "expert": 4},
-            batch=2,
-            seq=32,
-            block_size=1,
+            top_k=2,
+            capacity_factor=2,
+            mesh_spec={"fsdp": 1, "model": 4, "seq": 4, "expert": 4},
+            batch=4,
+            seq=2048,
             dtype=jnp.bfloat16,
         )[1]
-        self.helper_blockwise_gating_v2_vs_v1(cfg)
-
-    @unittest.skip("skip gather")
-    def test_unit_fwd_gather(self):
-        self.helper_fwd(self.create_cfg(test=TopKGatingGather, golden=TopKGating, test_device="cpu", golden_device="cpu", layer="gating"))
+    
+    def test_unit_fwd_blockwisev2_ep(self):
+        self.helper_blockwise_gating_v2_vs_v1(self.create_cfg(test=TopKGatingGatherBlockwiseV2, golden=TopKGatingGatherBlockwiseV2, test_device="cpu", layer="gating"))
+    
 
 class TestDev150bGatingInteg(GatingTestCase):
     def create_cfg(self, test, golden, test_device, golden_device="cpu", layer="gating"):
@@ -104,6 +103,27 @@ class TestDev150bGatingInteg(GatingTestCase):
     def test_integ_fwd_blockwisev2(self):
         self.helper_blockwise_gating(self.create_cfg(test=TopKGatingGatherBlockwiseV2, golden=None, test_device="neuron", layer="gating"))
 
+    def test_integ_fwd_blockwisev2_ep(self):
+        cfg = create_test_config(
+            layer="gating",
+            test=TopKGatingGatherBlockwiseV2,
+            golden=TopKGatingGatherBlockwise,
+            golden_device="cpu",
+            test_device="neuron",
+            input_dim=1024,
+            hidden_dim=4096,
+            n_experts=64,
+            n_groups=1,
+            top_k=1,
+            capacity_factor=1,
+            mesh_spec={"fsdp": -1, "model": 4, "seq": 4, "expert": 4},
+            batch=2,
+            seq=32,
+            block_size=1,
+            dtype=jnp.bfloat16,
+        )[1]
+        self.helper_blockwise_gating_v2_vs_v1(cfg)
+    
     @unittest.skip("skip gather")
     def test_integ_fwd_gather(self):
         self.helper_fwd(self.create_cfg(test=TopKGatingGather, golden=TopKGating, test_device="neuron", golden_device="cpu", layer="gating"))
